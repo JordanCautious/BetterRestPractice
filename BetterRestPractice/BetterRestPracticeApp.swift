de18a9fbfbae1bd85b551a3f79ca8fe704ded560
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-10 19:24:29 -0400

    Initial Commit

---
+++ BetterRestPractice/BetterRestPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestPracticeApp.swift
+//  BetterRestPractice
+//
+//  Created by Jordan Haynes on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
